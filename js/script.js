7a81629bbebb2782b3bcb1e8ec0fa00fc72740b2
eude313 <eudes.charles@student.moringaschool.com>
2021-06-20 01:32:30 +0300

    add functionality to the button

--- js/script.js
+++ js/script.js
@@ -7,3 +7,9 @@ $('.brand').click(function () {
 $(".blue").hover(function () {
     $(this).children(".red").slideToggle(1000, "linear");
 });
+
+$("button").click(function (Eudes) {
+    let client = document.getElementsByClass('nameDetail').value;
+    alert(' Dear ' + nameDetail + ' we have received your message successfully. ' + ' Reach out to us anytime');
+    Eudes.preventDefault();
+});
