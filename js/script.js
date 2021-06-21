c15917ab3e77ddfa7194967a236e956fc38cc986
eude313 <eudes.charles@student.moringaschool.com>
2021-06-21 17:48:52 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 
-$('.brand').click(function () {
-    $(this).children('.drop').slideToggle(1300);
-    $(this).children('.show').slidetoggle(1400);
-});  
+function create(i,p)  {
+    $ (document).ready(function(){
+        i.click(function() {
+            i.toggle();
+            p.toggle();
+        });
+        p.click(function(){
+            i.toggle();
+            p.toggle();
+        });
+    });
+   
+}
+create($('.brand'), $('.drop'));
+create($('.brand2'), $('.drop2'));
+create($('.brand3'), $('.drop3'));
 
 $(".range").hover(function () {
     $(this).children(".red").slideToggle(1000, "linear");
